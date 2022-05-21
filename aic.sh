0a212a26ff3177c058d0b59859f37e45eb353562
ak1ra26 <41eckfmc19d@gmail.com>
2022-05-21 08:13:04 +0300

    update

--- aic.sh
+++ aic.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-archinstall --config https://pastebin.com/raw/3FqFtQWJ
+
+echo 'Please enter your choice of config: '
+options=("default" "sway")
+select optpackages in "${options[@]}"
+do
+    case $optpackages in
+        "default")
+            conl="3FqFtQWJ"
+            break
+            ;;
+        "sway")
+            conl="g1VMkaeq"
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+archinstall --config https://pastebin.com/raw/$conl
+
+
+
