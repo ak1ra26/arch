bcb6aaaad6a6fbc9ae6c5a74ca67f3020cde2e3b
ak1ra26 <41eckfmc19d@gmail.com>
2022-12-11 12:54:57 +0200

    update

--- aic.sh
+++ aic.sh
@@ -12,7 +12,7 @@ do
             ;;
         "Lenovo T440")
             conl="0Qzyd9BF"
-            disl="--disk_layouts https://pastebin.com/raw/PVAi240a"
+#             disl="--disk_layouts https://pastebin.com/raw/PVAi240a"
             break
             ;;
         "sway")
@@ -23,7 +23,9 @@ do
         *) echo "invalid option $REPLY";;
     esac
 done
-archinstall --config https://pastebin.com/raw/$conl $disl
+curl https://githubusercontent.com/ak1ra26/arch/main/chroot.sh>chroot.sh
+chmod +x chroot.sh
+archinstall --config https://pastebin.com/raw/$conl #$disl
 
 
 
