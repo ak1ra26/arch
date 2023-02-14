cb51cb6c8fb206cc7de143908b85c4f1e65eabeb
ak1ra26 <41eckfmc19d@gmail.com>
2023-02-14 09:01:43 +0200

    update

--- yay.sh
+++ yay.sh
@@ -54,9 +54,10 @@ yvbox=""
 fi
 
 yay --save --answerdiff None --answerclean None --removemake
-yay -S google-chrome slack-desktop firefox-beta-bin authy windscribe-cli qt5-imageformats kimageformats plasma5-applets-eventcalendar $yvbox --noconfirm --disable-download-timeout #libpdfium megasync fix!!!!
+yay -S google-chrome slack-desktop firefox-beta-bin authy windscribe-cli qt5-imageformats kimageformats plasma5-applets-eventcalendar sni-qt $yvbox --noconfirm --disable-download-timeout
 yay -S spotify --noconfirm --disable-download-timeout
 
+#sni-qt is an optional dependencies for megasync. It fixes systray issue on KDE and LXQt
 # Це фікс помилки при встановленні Spotify because they release a new pkg with a different commit number and a different checksum. Дякую Joan31 за підказку. Але треба перевірити, бо писав цей код у блокноті на зміні, коли мене відволікали. Треба _commit=gc5f8b819 замінити на _commit=gc5f8b819-2, а 3cc25f28ae791ac26607117a5df668f803ed8e58f0ace085010a6242fdde97766bdc1c752560850795c9b4324f3e019937fe9af2788a1946ebb70ee781f50d99 на 9ba6c2d155f683b9a38222d58a2a53a2a5f4b422ed1c0d603af87919ba8a68309aea3354278fd1d5d8142a1568d93b7e83b14c041e749b0c39f3bc155a633ef8, щоб Spotify встановився.
 qspackage=spotify && pacmancheck
 if	[[ $pacmancheckres = 0 ]]; then
